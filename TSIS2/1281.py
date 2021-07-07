e53018f0251061b4591d4e516df6a328b5797b11
tssunami <64527096+RamazanMakhambetov@users.noreply.github.com>
2021-07-07 11:31:41 +0600

    Delete TSIS2 directory

--- TSIS2/1281.py
+++
@@ -1,4 +0,0 @@
-class Solution(object):
-    def subtractProductAndSum(self, n):
-        n = [int(digit) for digit in str(n)]
-        return functools.reduce(lambda x, y: x * y, n) - sum(n)
--- TSIS2/1678.py
+++
@@ -1,3 +0,0 @@
-class Solution(object):
-    def interpret(self, command):        
-        return command.replace('()','o').replace('(al)','al')
