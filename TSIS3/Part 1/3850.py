b2a04ef7f6a680de791a20278a50373756410579
tssunami <64527096+RamazanMakhambetov@users.noreply.github.com>
2021-07-07 11:31:49 +0600

    Delete TSIS3 directory

--- TSIS3/Part 1/3850.py
+++
@@ -1,3 +0,0 @@
-arr = list(map(int, input().split()))
-print(*[arr[i] for i in range(len(arr)) if arr[i] != 0],
-      *[arr[i] for i in range(len(arr)) if arr[i] == 0])
--- TSIS3/Part 1/3853.py
+++
@@ -1,3 +0,0 @@
-loop = list(map(int, input().split()))
-steps = int(input()) % len(loop)
-print(*loop[-steps::] + loop[:-steps])
